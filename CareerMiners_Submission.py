8c180786900cdd147f7a8bdbf317703ba4fc3389
chorajesh <chorajesh>
2024-06-10 22:01:34 +0000

    Updated the Documentation

--- CareerMiners_Submission.py
+++ CareerMiners_Submission.py
@@ -1,13 +1,84 @@
 # Databricks notebook source
+# MAGIC %sql
+# MAGIC -- create table workspace.default.posting_cleaned
+# MAGIC -- using delta as
+# MAGIC create or replace temp view posting_no_nulls as
+# MAGIC
+# MAGIC select job_id, company_name, title, description from workspace.default.postings
+# MAGIC where job_id is not null and company_name is not null and title is not null and description is not null
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC create table workspace.default.posting_cleaned
+# MAGIC using delta as
+# MAGIC
+# MAGIC with cte as(select job_id, count(job_id) from posting_no_nulls
+# MAGIC group by 1
+# MAGIC having count(job_id) > 1
+# MAGIC )
+# MAGIC select * from posting_no_nulls
+# MAGIC where job_id not in (select job_id from cte)
+
+# COMMAND ----------
+
+# DBTITLE 1,checking if there are any nulls
+# MAGIC %sql
+# MAGIC select job_id, count(job_id) from workspace.default.posting_cleaned
+# MAGIC group by 1
+# MAGIC having count(job_id) > 1
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select count(*) from workspace.default.posting_cleaned
+
+# COMMAND ----------
+
+# DBTITLE 1,Formatted dataset
+# MAGIC %sql
+# MAGIC select * from workspace.default.posting_cleaned
+
+# COMMAND ----------
+
+# DBTITLE 1,Create Vector Index for the above table
+from databricks import vector_search
+
+# Define the parameters for the vector index creation
+index_name = "career_miners_description_index"
+source_table = "workspace.default.posting_cleaned"
+text_column = "description"
+embedding_model = "databricks-bge-large-en"
+options = {
+    "delta_sync": "true",
+    "schema_evolution": "true"
+}
+
+# Create the vector index using the Databricks Foundation Embedding model
+vector_search.create_vector_index(
+    index_name=index_name,
+    source_table=source_table,
+    text_columns=[text_column],
+    embedding_model=embedding_model,
+    options=options
+)
+
+# COMMAND ----------
+
+# DBTITLE 1,Add libraries
 !pip install mlflow[genai]>=2.9.0
 !pip install databricks-vectorsearch
 
 # COMMAND ----------
 
+# DBTITLE 1,Restart the Databricks Kernel After Installing the Packages
 dbutils.library.restartPython()
 
 # COMMAND ----------
 
+# DBTITLE 1,Import Necessary Packages
 import mlflow.deployments
 import json
 import os
@@ -17,6 +88,7 @@ from databricks.vector_search.client import VectorSearchClient
 
 # COMMAND ----------
 
+# DBTITLE 1,Using dbrx model for getting the required skills for the user input
 def getLLMResponse(userinput):
     client = mlflow.deployments.get_deploy_client("databricks")
     endpoints = client.list_endpoints() 
@@ -42,6 +114,7 @@ def getLLMResponse(userinput):
 
 # COMMAND ----------
 
+# DBTITLE 1,Get the User Skills Based on the user Input
 def getUserSkill(userinput):
     response = getLLMResponse(userinput)
     skills = json.loads(response)
@@ -54,6 +127,7 @@ def getUserSkill(userinput):
 
 # COMMAND ----------
 
+# DBTITLE 1,initialization and results
 def getJobs(userinput, noOfJobs):
 
     workspace_url = os.environ.get("WORKSPACE_URL")
@@ -75,15 +149,16 @@ def getJobs(userinput, noOfJobs):
 
 # COMMAND ----------
 
+# DBTITLE 1,Results from our dataset for the relevent skills
 def showjobs(userinput, noOfJobs):
     job_results = getJobs(userinput, noOfJobs)
-    print("Top 5 matches on the user query:")
+    print(f"Top {noOfJobs} matches on the user query:")
     print("\n\n----------------------------------------------")
     print("\nJOB_ID \t Title \t Company \t Job Description \t Relevancy Score")
     print("\n----------------------------------------------")
     for job in job_results:
         #column_index = job[0].index("job_id")
-        print(f"{job[0]} \t {job[1]} \t {job[2]} \t {job[3]} \t {job[4]}")
+        print(f"{str(job[0])} \t {job[1]} \t {job[2]} \t {job[3]} \t {job[4]}")
         print("\n")
     return job_results
 
